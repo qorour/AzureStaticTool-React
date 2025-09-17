894925ce102e7f76f34f526e6dccc013071dddf4
Quinn <qo.personalbusiness@gmail.com>
2025-09-17 10:49:46 -0500

    testing image load

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import ren_logo from './new_logo.gif';
 
 function App() {
-  const value = 'World';
-  return <div>Hello there, {value}!</div>;
+  const value = 'Quinn';
+  return (
+    <div className="Homepage">
+      <div>Hello there, {value}!</div>
+      <img src={ren_logo} alt="Renishaw image" height={200} width={200} />
+    </div>
+  );
 }
 
 export default App;
+
